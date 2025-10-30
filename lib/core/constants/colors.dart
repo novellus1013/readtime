05541f81d60b1715bec69dcd605c7e8cd6649f01
novellus1013 <novellus1013@gmail.com>
2025-10-30 21:46:23 +0900

    home_screen ui, constants

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+Color primaryColor = const Color(0xFF101922);
+Color darkBlue = const Color(0xFF2A4181);
+Color lightBlue = const Color(0xFF1373D4);
+Color lightGreen = const Color(0xFF4A6282);
+Color lightOrange = const Color(0xFFFF7F51);
+Color lightWhite = const Color(0xFFFFFFFF);
+Color tealBlue = const Color(0xFFB2DFDB);
---
+++ lib/ui/screens/home_screen.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:read_time/core/constants/colors.dart';
+import 'package:read_time/core/constants/gaps.dart';
+import 'package:read_time/core/constants/sizes.dart';
+
+class HomeScreen extends StatelessWidget {
+  final int goalSessionTime = 180;
+  final int todaysSessionTime = 20;
+  final int lastSessionTime = 35;
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(Sizes.size96),
+        child: AppBar(
+          backgroundColor: primaryColor,
+          foregroundColor: lightWhite,
+          title: Row(children: [
+            Image.asset(
+              'assets/images/read_time_logo.png',
+              height: Sizes.size80 + Sizes.size14,
+            ),
+            const Text(
+              'Read Time',
+              style: TextStyle(
+                fontSize: Sizes.size28,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ]),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.settings_outlined,
+                size: Sizes.size28,
+              ),
+            )
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size24,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const _StartButton(),
+            Gaps.v60,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _HomeCard(
+                  title: "일일 목표",
+                  icon: FontAwesomeIcons.fire,
+                  time: "$todaysSessionTime / $goalSessionTime 분",
+                ),
+                Gaps.h20,
+                _HomeCard(
+                  title: "마지막 세션",
+                  icon: FontAwesomeIcons.arrowAltCircleLeft,
+                  time: "$lastSessionTime 분",
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _HomeCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final String time;
+
+  const _HomeCard({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.time,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          decoration: BoxDecoration(
+            color: darkBlue,
+            borderRadius: BorderRadius.circular(
+              Sizes.size10,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(
+              Sizes.size14,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                        color: tealBlue,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    FaIcon(
+                      icon,
+                      size: Sizes.size16,
+                      color: lightOrange,
+                    ),
+                  ],
+                ),
+                Gaps.v24,
+                Text(
+                  time,
+                  style: TextStyle(
+                    color: lightWhite,
+                    fontSize: Sizes.size16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// class _DailyGoal extends StatelessWidget {
+//   const _DailyGoal({
+//     super.key,
+//     required this.todaysSessionTime,
+//     required this.goalSessionTime,
+//   });
+
+//   final int todaysSessionTime;
+//   final int goalSessionTime;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Expanded(
+//       child: AspectRatio(
+//         aspectRatio: 1,
+//         child: Container(
+//           decoration: BoxDecoration(
+//             color: darkBlue,
+//             borderRadius: BorderRadius.circular(
+//               Sizes.size10,
+//             ),
+//           ),
+//           child: Padding(
+//             padding: const EdgeInsets.all(
+//               Sizes.size14,
+//             ),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Row(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   children: [
+//                     Text(
+//                       '일일 목표',
+//                       style: TextStyle(
+//                         color: tealBlue,
+//                         fontWeight: FontWeight.w500,
+//                       ),
+//                     ),
+//                     FaIcon(
+//                       FontAwesomeIcons.fire,
+//                       size: Sizes.size16,
+//                       color: lightOrange,
+//                     ),
+//                   ],
+//                 ),
+//                 Gaps.v24,
+//                 Text(
+//                   '$todaysSessionTime / $goalSessionTime 분',
+//                   style: TextStyle(
+//                     color: lightWhite,
+//                     fontSize: Sizes.size16,
+//                     fontWeight: FontWeight.w500,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class _StartButton extends StatelessWidget {
+  const _StartButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double buttonSize = MediaQuery.of(context).size.width * 0.8;
+
+    return ElevatedButton(
+      onPressed: () {
+        print("눌렸어용");
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: darkBlue,
+        foregroundColor: lightWhite,
+        minimumSize: Size(buttonSize, buttonSize),
+      ),
+      child: const Text(
+        'Start',
+        style: TextStyle(
+          fontSize: Sizes.size48,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    );
+  }
+}
